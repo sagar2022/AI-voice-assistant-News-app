bf844976c78758b582185e5853b1e19fa17b02fa
sagar <chaudharysagar256@gmail.com>
2022-02-03 23:35:45 +0545

    AI News App

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import wordsToNumbers from "words-to-numbers";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles.js";
+
+const alanKey =
+  "11f2a17ae78191458e386a3d434ccb442e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+function App() {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://github.com/sagar2022/myPortfolio/blob/main/images/logo1.svg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>
+  );
+}
+
+export default App;
